a320ffb246a2f287dc79c0f53a83518fa3f776b1
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-13 00:15:11 +0300

    Добавили задачу 04

---
+++ Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Напишите программу, которая принимает на вход
+// трехзначное число и на выходе показывает
+// последнюю цифру этого числа
+
+Console.WriteLine("ВВедите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+{
+    num = num * -1;
+}
+if(num >= 100 && num <=999)
+{
+    int lastDigit = num % 10;
+    Console.WriteLine($"Последняя цифра числа: {lastDigit}");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
