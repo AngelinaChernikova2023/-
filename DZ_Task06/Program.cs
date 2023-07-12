4fdda306cf0c565ecdf9d83bbcb44d8ea9a70215
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-13 01:19:06 +0300

    Решила задачу ДЗ06

--- DZ_Task06/Program.cs
+++ DZ_Task06/Program.cs
@@ -5,3 +5,11 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0) 
+System.Console.WriteLine($"Число: {num} четное");
+else System.Console.WriteLine($"Число: {num} нечетное");
+
