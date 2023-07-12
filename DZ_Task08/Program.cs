675e242d9ce7e1b7d756c69198ceb2425abb77d9
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-13 00:45:57 +0300

    Решила задачу ДЗ08

--- DZ_Task08/Program.cs
+++ DZ_Task08/Program.cs
@@ -4,3 +4,19 @@
 // все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое положительное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 0)
+{
+    Console.WriteLine("Ошибка ввода");
+}
+else
+{ 
+    int counter = 2;
+    while(counter <= num)
+    {
+        Console.Write(counter + " ");
+        counter = counter + 2;
+    }
+}
