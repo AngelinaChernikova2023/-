e9db252b490b072a73e121c5012dd31cd709a6a2
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-12 23:09:11 +0300

    Добавили задачу 01

---
+++ Task01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая 
+// 1. на вход принимает два числа 
+// 2. и проверяет является ли певрое число квадратом второго.
+// например: a = 25, b = 5 -> Да
+
+Console.WriteLine ("Введите первое целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num == num1 * num1)
+{
+ Console.WriteLine ( "Первое число является квадратом второго" );
+}
+else
+{
+    Console.WriteLine ( "Первое число не является квадратом второго" );
+}
+
