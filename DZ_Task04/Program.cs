d30e48fe4e6bb27c7e057291ae844f153bb88f75
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-13 01:24:00 +0300

    Решила задачу ДЗ04

--- DZ_Task04/Program.cs
+++ DZ_Task04/Program.cs
@@ -4,3 +4,15 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.WriteLine("Введите число 1");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num;
+if (num > max) max = num;
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+Console.WriteLine($"max = {max}");
