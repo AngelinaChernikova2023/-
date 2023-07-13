b0e02d4e712dcdc0f88ffa23f839a720d3cd75f5
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-13 21:24:57 +0300

    Решили задачу 09

---
+++ Task09/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит
+// случайное число из диапазона [10 - 99] 
+// и показывает наибольшую цифру числа
+
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из диапазона 10 - 99 -> {num}");
+int firstDigit = num / 10; // 78 / 10 = 7
+int secondDigit = num % 10; // 78 % 10 = 8
+if (firstDigit > secondDigit)
+Console.WriteLine($"Максимальная цифра числа {firstDigit}");
+else Console.WriteLine($"Максимальная цифра числа {secondDigit}");
