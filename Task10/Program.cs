6c433bd87a58078f9ae5da98e5d87b692509a96e
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-13 21:43:54 +0300

    Решили задачу 11

---
+++ Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число от 100 до 999 {number}");
+int firstDight = number / 100;
+int thirdDight = number % 10;
+int resultDight = firstDight * 10 + thirdDight;
+Console.WriteLine($"Получено двузначное число {resultDight}");
+
+//ИЛИ
+//int RemoveSecondDigit (int number)
+//{
+//    int firstDight = number/100;
+//int thirdDight = number % 10;
+//int resultDight = firstDight *10 + thirdDight;
+//return resultDight;
+//}
