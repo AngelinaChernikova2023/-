ee69f3abb43a0950c6e5da858e95e008451afae4
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-15 02:51:50 +0300

    Решила задачу ДЗ13

--- DZ_Task13/Program.cs
+++ DZ_Task13/Program.cs
@@ -9,43 +9,65 @@ int number = int.Parse(Console.ReadLine());
 
 if (number < 100)
 {
-Console.WriteLine("Третьей цифры нет");
+    Console.WriteLine("Третьей цифры нет");
 }
-if (number >= 100 && number <=999)
+if (number >= 100 && number <= 999)
 {
     int thirdNumber = number % 10;
     Console.WriteLine($"Третья цифра числа: {thirdNumber}");
 }
-else 
-{   
-    while (number >= 100 && number <=999)
+// if
+// {   
+//     while(number >= 100 && number <=999)
+// {
+//     Console.WriteLine($"Третья цифра числа: {number}");
+//     return;
+// }
+// }
+else
 {
-    int number1 = number / 10;
-    Console.WriteLine($"Третья цифра числа: {number1}");
+    int result = -1;
+    while (number > 999)
+    {
+       number = number / 10;
+    }  
+    result = number % 10;
+
+    Console.WriteLine($"Третья цифра числа: {result}");
     return;
 }
-}
 
+// int number = ReadInt("Введите число: ");
+// int count = number.ToString().Length;
+// Console.Write(MakeArray(number, count));
+
+
+// // ФУНКЦИИ------------------------------------------------------------------------------------------------------
+
+// // Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
+// int ReadInt(string message)
 // {
-//     while 
-//         number /= 10;
-//     }
-//     return number % 10;
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
 // }
-// // int count = GetCountDigits(number);
-// // Console.Write($"Количество цифр в числе {number} = {count}");
 
-// // int GetCountDigits(int number);
-// // while (count <= 3);
-// // {
-// //     if (number == 0) return 1;
+// // Функция принимает число введенное пользователем, количество символов, и выводит третью цифру числа. Если 3 цифры нет, сообщает и выводит 0.
+// int MakeArray(int a, int b)
+// {
+// int result = 0;
+//     if (b < 3)
+//     {
+//         Console.Write("Третьей цифры нет, держи: ");
+//     }
+//     else
+//     {
+//         int c = 1;
+//         for (int i = b; i > 3; i--)
+//         {
+//             c = c * 10;
+//         }
 
-// //     int counter = 0;
-// //     while (number  != 0)
-// //     {
-// //         number /= 10; // num = num / 10
-// //         counter++;
-// //     }
-// //     return counter;
-// // }
-// // int Console.WriteLine( {GetCountDigits} );
+//         result = (a / c) % 10;
+//     }
+// return result;
+// }
