297526b4df71d42ace379a990bdcff8f4c2d0327
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-15 00:49:55 +0300

    Решила задачу 10

---
+++ DZ_Task10/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number = number * -1;
+}
+if (number >= 100 && number <=999)
+{
+int firstDight = number / 10;
+int secondDight = firstDight % 10;
+Console.WriteLine($"Вторая цифра числа: {secondDight}");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
+
+// {
+// int firstDight = number / 100;
+// int thirdDight = number % 10;
+// int secondDight = firstDight * 10 + thirdDight;
+// Console.WriteLine($"Вторая цифра числа: {secondDight}");
+// }
