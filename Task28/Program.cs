5ffae16e7a4a8496abb77ca241d891714afcb76f
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-14 21:09:24 +0300

    Решили задачу 28

---
+++ Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая
+// принимает на вход число (А) и выдает произведение чисел
+// от 1 до А.
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {factorial}");
+
+int Factorial(int num)
+
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+          fact *= i; // fact = fact * i
+        }
+      
+    }
+return fact;
+}
