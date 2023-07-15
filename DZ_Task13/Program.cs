f9eaf6e60aecb0f8be537572882dc9f3d26ebf5a
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-15 12:58:05 +0300

    Решила задачу ДЗ13

--- DZ_Task13/Program.cs
+++ DZ_Task13/Program.cs
@@ -16,10 +16,9 @@ if (number >= 100 && number <= 999)
     int thirdNumber = number % 10;
     Console.WriteLine($"Третья цифра числа: {thirdNumber}");
 }
-else
+if (number >= 100)
 {
     int result = -1;
-    if (number >= 100)
     {
     while (number > 999)
     {
@@ -31,37 +30,18 @@ else
     return;
 }
 
-// int number = ReadInt("Введите число: ");
-// int count = number.ToString().Length;
-// Console.Write(MakeArray(number, count));
-
-
-// // ФУНКЦИИ------------------------------------------------------------------------------------------------------
-
-// // Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
-// int ReadInt(string message)
 // {
-//     Console.Write(message);
-//     return Convert.ToInt32(Console.ReadLine());
+//     int result = -1;
+//     if (number >= 100)
+//     {
+//     while (number > 999)
+//     {
+//        number = number / 10;
+//     }  
+//     result = number % 10;
+//     }
+//     Console.WriteLine($"Третья цифра числа: {result}");
+//     return;
 // }
 
-// // Функция принимает число введенное пользователем, количество символов, и выводит третью цифру числа. Если 3 цифры нет, сообщает и выводит 0.
-// int MakeArray(int a, int b)
-// {
-// int result = 0;
-//     if (b < 3)
-//     {
-//         Console.Write("Третьей цифры нет, держи: ");
-//     }
-//     else
-//     {
-//         int c = 1;
-//         for (int i = b; i > 3; i--)
-//         {
-//             c = c * 10;
-//         }
 
-//         result = (a / c) % 10;
-//     }
-// return result;
-// }
