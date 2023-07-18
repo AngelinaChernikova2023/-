8ffeade9d6fcdb394086a3aba042028a2baf544a
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-19 00:15:24 +0300

    Решила задачу ДЗ25

---
+++ DZ_Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на  вход два числа
+// (А и В) и возводит число А в натуральную степень В.
+// Например:
+// 3, 5 -> 243
+
+Console.WriteLine("Введите целое число А");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число В");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+double Square = Math.Pow(num, num1);
+Console.WriteLine ($"Квадрат числа А возведенный в степень числа В = {Square}");
+
