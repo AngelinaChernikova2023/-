c364d814149b4e06171936ba113b2ab550197baa
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-19 01:05:01 +0300

    Решила задачу ДЗ23

---
+++ DZ_Task23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите целое число А");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write(«Таблица кубов:\n»);
+// for (int i = 1; i <= n; i++)
+// {
+//     int k = i * i * i;
+//     Console.Write($»{ k}, «);
+// }
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i * i * i} ");
+}
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
