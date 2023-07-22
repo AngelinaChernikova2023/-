05c396c629ef89817804eaefca1e9d35e1426fe1
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-22 13:12:56 +0300

    Решили задачу 22

---
+++ Task22/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N)
+// и выдает таблицу квадратов чисел
+// от 1 до N
+
+Console.WriteLine("Введите целое число А");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write(«Таблица квадратов:\n»);
+// for (int i = 1; i <= n; i++)
+// {
+//     int k = i * i;
+//     Console.Write($»{ k}, «);
+// }
+
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine($"{i * i} ");
+}
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
