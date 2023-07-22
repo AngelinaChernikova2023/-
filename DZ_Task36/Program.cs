01e0ec827031c19941d28e6566145166aec5751c
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-22 19:27:09 +0300

    Решила задачу ДЗ36

---
+++ DZ_Task36/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте одномерный массив, 
+// заполненный случайными числами. 
+// Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray (int[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.Write("]    ");
+}
+
+Console.WriteLine();
+
+int[] array = CreateArrayRndInt(10, 0, 10);
+PrintArray(array);
+
+Console.WriteLine();
+
+int SumNotEvenElem(int[] arr)
+{
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i % 2 != 0) sum += arr[i];
+}
+return sum;
+}
+int sum = SumNotEvenElem(array);
+Console.WriteLine($"Сумма не четных элементов массива равна {sum}");
