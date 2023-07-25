ccdb2b475373c55d52f19a23232a4bff8959e168
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-25 14:23:30 +0300

    Решили задачу 42

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -5,18 +5,31 @@
 Console.WriteLine("Введите число, которое необходимо перевести в двоичную систему: ");
 int number = int.Parse(Console.ReadLine());
 
-int result = DecToBin(number);
-Console.WriteLine($"Число в двоичном представлении = {result}");
+// int result = DecToBin(number);
+// Console.WriteLine($"Число в двоичном представлении = {result}");
 
-int DecToBin(int number)
+// int DecToBin(int number)
+// {
+//     int numResult = 0;
+//     int decimalBase = 1;
+//     while (number > 0) 
+//     {
+//       numResult += number % 2 * decimalBase;
+//       number /=2;
+//       decimalBase *=10;
+//     }
+//     return numResult;
+// }
+
+Console.WriteLine($"Число в двоичном представлении = {DecToBinStr(number)}");
+
+string DecToBinStr(int num)
 {
-    int numResult = 0;
-    int decimalBase = 1;
-    while (number > 0) 
+    string numResult = string.Empty;
+        while (num > 0) 
     {
-      numResult += number % 2 * decimalBase;
-      number /=2;
-      decimalBase *=10;
+      numResult = num % 2 + numResult;
+      num /=2;
     }
     return numResult;
 }
