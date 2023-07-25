03d3da20a3bed95ac1bf3ef0251cf989d40920fd
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-25 14:10:31 +0300

    Решили задачу 42

---
+++ Task42/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая
+// будет преобразовывать десятичное число в двоичное
+// Делим на два, пока не дойдет до нуля
+
+Console.WriteLine("Введите число, которое необходимо перевести в двоичную систему: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = DecToBin(number);
+Console.WriteLine($"Число в двоичном представлении = {result}");
+
+int DecToBin(int number)
+{
+    int numResult = 0;
+    int decimalBase = 1;
+    while (number > 0) 
+    {
+      numResult += number % 2 * decimalBase;
+      number /=2;
+      decimalBase *=10;
+    }
+    return numResult;
+}
