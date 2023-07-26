e3ade4c92666a9afd2949ea7fcbb3f3c9757389e
Angelina Chernikova <karlova_angelina@mail.ru>
2023-07-26 15:18:32 +0300

    Решили задачу 44

---
+++ Task44/Program.cs
@@ -0,0 +1,40 @@
+﻿// Не используя рекурсию, 
+// выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 01123
+// Если N = 7 -> 0112358
+
+// int size = Convert.ToInt32(Console.ReadLine());
+
+int[] Fibonacci(int num)
+{
+    int[] array = new int[num];
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i-1] + array[i-2];
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+
+    Console.Write("[ ");
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+        if (i == arr.Length - 1)
+        {
+            break;
+        }
+        Console.Write(" ");
+    }
+    Console.WriteLine(" ]    ");
+}
+
+
+Console.WriteLine("Введите размер для массива");
+int size = Convert.ToInt32(Console.ReadLine());
+PrintArray(Fibonacci(size));
